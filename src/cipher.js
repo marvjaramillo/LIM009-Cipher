2c790f8a3b8c0d85615c131b344afc89a325f887
13EstMaria <20170766@aloe.ulima.edu.pe>
2019-03-01 10:52:33 -0500

    actulizacion 1

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,42 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (nortxt, offset1) => {
+
+    let output = '';
+    let valOffset = parseInt(offset1);
+
+    for(let i = 0; i < nortxt.length; i++) {
+      const codigoLetra = nortxt.charCodeAt(i);
+      if(codigoLetra >= 65 && codigoLetra <= 90) {
+        //mayúsculas
+        output += String.fromCharCode((codigoLetra - 65 + valOffset) % 26 + 65 );
+      } else if (codigoLetra === 32) {
+      //espacio  
+        output += nortxt[i];
+      } else if (codigoLetra >= 97 && codigoLetra <= 122) {
+      //minúsculas
+        output += String.fromCharCode((codigoLetra - 97 + valOffset) % 26 + 97);
+      }
+    }
+    return output;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (nortxt2,offset2) => {
+
+    let result = '';
+    let valOffset1 = parseInt(offset2);
+
+    for(let c = 0; c < nortxt2.length; c++) {
+      const codigoletra1 = nortxt2.charCodeAt(c);
+      if(codigoletra1 >= 65 && codigoletra1 <= 90) {
+        //mayúsculas
+        result += String.fromCharCode((codigoletra1 + 65 - valOffset1) % 26 + 65);
+      } else if (codigoletra1 === 32) {
+        //espacio
+        result += nortxt2[c];
+      } else if (codigoletra1 >= 97 && codigoletra1 <= 122) {
+        //minúsculas
+        result += String.fromCharCode((codigoletra1 + 7 - valOffset1) % 26 + 97);
+      }
+    }
+    return result;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 /* Acá va tu código */
+window.onload = () => {
+    document.getElementById('second').style.display='none';
+    document.getElementById('third').style.display='none';
+}
+
+document.getElementById('start').addEventListener('click', () => {
+    document.getElementById('first').style.display='none';
+    document.getElementById('second').style.display='block';
+    document.getElementById('third').style.display='none';
+});
+
+let cif = document.getElementById('cipherbox');
+let offset = document.getElementById('shifter');
+let cifh2 = document.getElementById('studentcipher');
+let offseth3 = document.getElementById('decipheroffset');
+
+document.getElementById('profesor').addEventListener('click', () => {
+    cifh2.innerHTML=cif.value;
+    offseth3.innerHTML= offset.value;
+    document.getElementById('first').style.display='none';
+    document.getElementById('second').style.display='none';
+    document.getElementById('third').style.display='block';
+});
+
+document.getElementById('again').addEventListener('click', () => {
+    document.getElementById('first').style.display='none';
+    document.getElementById('second').style.display='block';
+    document.getElementById('third').style.display='none';
+});
+
+let nortxt = document.getElementById('normal');
+let enctxt = document.getElementById('ciphertxt');
+let offset1 = document.getElementById('shift1');
+
+offset1.addEventListener('input', () => {
+    enctxt.value = cipher.encode(nortxt.value, offset1.value);
+});
+
+let nortxt2 = document.getElementById('cifradotxt');
+let enctxt1 = document.getElementById('answerciphertxt');
+let offset2 = document.getElementById('shift2');
+
+offset2.addEventListener('input', () => {
+    enctxt1.value = cipher.decode(nortxt2.value, offset2.value);
+});
