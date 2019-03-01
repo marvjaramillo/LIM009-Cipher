12d3aad03f62bb95774a97c9a62dd5d4ab0380d3
13EstMaria <20170766@aloe.ulima.edu.pe>
2019-03-01 13:09:36 -0500

    actualizacion 2

--- src/index.js
+++ src/index.js
@@ -32,15 +32,19 @@ document.getElementById('again').addEventListener('click', () => {
 let nortxt = document.getElementById('normal');
 let enctxt = document.getElementById('ciphertxt');
 let offset1 = document.getElementById('shift1');
+let mostrarnumber = document.getElementById('numbershift');
 
 offset1.addEventListener('input', () => {
     enctxt.value = cipher.encode(nortxt.value, offset1.value);
+    mostrarnumber.innerHTML = "# Desplazamientos:  " + offset1.value;
 });
 
 let nortxt2 = document.getElementById('cifradotxt');
 let enctxt1 = document.getElementById('answerciphertxt');
 let offset2 = document.getElementById('shift2');
+let mostrarnumber1 = document.getElementById('numbershift1');
 
 offset2.addEventListener('input', () => {
     enctxt1.value = cipher.decode(nortxt2.value, offset2.value);
+    mostrarnumber1.innerHTML = "# Desplazamientos:  " + offset2.value;
 });
